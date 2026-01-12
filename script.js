d38b516412000a19e74bb3f50f2677d7ad48d290
Theyah08 <aliyah.piel@jmc.edu.ph>
2026-01-12 23:54:48 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+
 function toggleMenu() {
     const nav = document.getElementById("navLinks");
     nav.style.display = nav.style.display === "flex" ? "none" : "flex";
 }
+
+window.onload = function() {
+    var petImages = [
+        "image/cats.png",
+        "image/Dogs.png",
+        "image/bird.png",
+        "image/fish.png",
+        "image/Rep.png"
+    ];
+
+    var currentIndex = 0;
+    var petImgElement = document.getElementById("pet-feature-img");
+
+    setInterval(function() {
+        // fade out
+        petImgElement.style.opacity = 0;
+
+        setTimeout(function() {
+            // change image after fade out
+            currentIndex = (currentIndex + 1) % petImages.length;
+            petImgElement.src = petImages[currentIndex];
+            // fade in
+            petImgElement.style.opacity = 1;
+        }, 500); 
+    }, 5000); 
+};
+
